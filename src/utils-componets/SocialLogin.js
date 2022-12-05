78714e2f920ed523905a3427431c033ed296b616
tetra47 <logesh.kannan@codeshastra.com>
2022-12-05 12:58:04 +0530

    social login

--- src/utils-componets/SocialLogin.js
+++ src/utils-componets/SocialLogin.js
@@ -5,10 +5,28 @@ import { useNavigate } from 'react-router-dom';
 import facebookIcon from '../assets/images/Facebook-icon.svg';
 import googleIcon from '../assets/images/Google-icon.svg';
 import { signInWithFacebook, signInWithGoogle } from '../firebase/firebaseAuth';
+import { firebase } from '../firebase/firebase';
+import ApiService from '../services/ApiService';
+import { toast } from 'react-toastify';
+import { setLoading } from '../redux/actions/LoaderActions';
+
 
 const SocialLogin = ({ setFieldValue }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const checkIfUserExists = async (email, phone) => {
+    const result = await ApiService('user/check-exists', 'POST', { email, phone }, true);
+    return result?.data?.data?.user;
+  }
+
+  const configureCaptcha = () => {
+    return (window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      size: 'invisible',
+      callback: (response) => {},
+      defaultCountry: 'IN',
+    }));
+  };
 
   const dualAuth = async (userDetail) => {
     let data = {
@@ -16,13 +34,37 @@ const SocialLogin = ({ setFieldValue }) => {
       email: userDetail?.email,
       fullName: userDetail?.displayName
     };
-    navigate('/signup', {
-      state: data
-    });
-    // const response = await ApiService('user/create', 'POST', data);
-    // if (response?.data?.code === 200) {
-    //   navigate('/dashboard');
-    // }
+    const user = await checkIfUserExists(userDetail?.email, null);
+    if(user?.uid) {
+        const appVerifier = configureCaptcha();
+        // dispatch(setLoading(true))
+        firebase
+          .auth()
+          .signInWithPhoneNumber(`${user.phone}`, appVerifier)
+          .then(async (confirmationResult) => {
+            window.confirmationResult = confirmationResult;
+            // dispatch(setLoading(false))
+            toast.success('OTP has been Sent to Mobile Number', {
+              theme: 'colored',
+            });
+    
+            navigate('/signin-otp', {
+              state: {
+                phoneNumber: user.phone,
+              },
+            });
+          })
+          .catch((error) => {
+            toast.error(`${error}`, {
+              theme: 'colored',
+            });
+            dispatch(setLoading(false));
+          });
+    } else {
+      navigate('/signup', {
+        state: data
+      });
+    }
   };
   return (
     <>
