683b64f0c583b3d7aae48c1e68ec16af4f2aa68a
Velmurugan K <velmurugan.k@codeshastra.com>
2022-12-10 16:10:03 +0530

    page num

--- src/pages/courses/application/CourseApply.js
+++ src/pages/courses/application/CourseApply.js
@@ -129,7 +129,7 @@ const CourseApplication = () => {
   });
 
   const nextPageNumber = (pageNumber) => {
-    switch (pageNumber = 5) {
+    switch (pageNumber) {
       case 0:
         setPage(0);
         setStepperTitle('Personal Details');
