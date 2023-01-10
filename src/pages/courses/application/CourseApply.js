ac78b663dd482b0f1831f5f7ba6f7bd9080fcb3e
Velmurugan K <velmurugan.k@codeshastra.com>
2023-01-10 12:31:34 +0530

    phone num enabled

--- src/pages/courses/application/CourseApply.js
+++ src/pages/courses/application/CourseApply.js
@@ -373,7 +373,7 @@ const CourseApplication = () => {
                           countryCodeEditable={false}
                           onBlur={formik.handleBlur('mobile_number')}
                           placeholder="Enter your Mobile number"
-                          disabled={ userData?.phone }
+                          // disabled={ userData?.phone }
                         />
                         {formik.touched.mobile_number && formik.errors.mobile_number ? (
                           <div className="error-message">{formik.errors.mobile_number}</div>
