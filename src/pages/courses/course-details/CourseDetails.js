57008e430c5b8b7a2534ec4ebfd67c7a43f99eb3
Velmurugan K <velmurugan.k@codeshastra.com>
2023-01-14 10:08:10 +0530

    issue fixed

--- src/pages/courses/course-details/CourseDetails.js
+++ src/pages/courses/course-details/CourseDetails.js
@@ -72,7 +72,7 @@ function CourseDetails() {
   };
 
   const apply = (course) => {
-
+console.log(course?.target_audience);
     if ( course?.target_audience === "{Learners}" ) {
       setopenpayment(true);
     } else {
