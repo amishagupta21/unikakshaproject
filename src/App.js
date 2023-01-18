c5eb417b421bebba188a098dda0be77e61b76aca
tetra47 <logesh.kannan@codeshastra.com>
2023-01-18 15:02:14 +0530

    footer-bottom

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ const App = () => {
       <BrowserRouter>
         <PrimaryNavbar />
         { toaster?.show && <Toaster header={toaster?.header} variant={toaster?.variant} body={toaster?.body} show={toaster?.show}/> }
+        <div style={{minHeight: '75vh'}}>
         <Routes>
           <Route exact={true} path="/" element={<Login />} />
           <Route element={<PrivateRoute />}>
@@ -58,6 +59,7 @@ const App = () => {
           <Route path="signup-otp" element={<SignupOtp />} />
           <Route path="*" element={<Error />} />
         </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
