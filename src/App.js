3293ef7c2eebdb35c4eb81e6f8278ecd73f67ca7
aamna <aamna.puri@codeshastra.com>
2023-02-20 19:12:04 +0530

    ui-fixes

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
     <div>
       {isLoader && <Loader />}
       <BrowserRouter>
+      <div className='main-sidebar'>
         <PrimaryNavbar />
         { toaster?.show && <Toaster header={toaster?.header} variant={toaster?.variant} body={toaster?.body} show={toaster?.show}/> }
         <div style={{minHeight: '76vh'}}>
@@ -61,6 +62,7 @@ const App = () => {
           <Route path="signup-otp" element={<SignupOtp />} />
           <Route path="*" element={<Error />} />
         </Routes>
+        </div>
         </div>
         <Footer />
       </BrowserRouter>
