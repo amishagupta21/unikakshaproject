b37e2c7f4bef99a1dd5e869ebf3ce1a4a8c3fc7b
USER <amisha.gupta@codeshastra.com>
2023-08-19 11:32:56 +0530

    stepper issue

--- src/pages/courses/application/FormProgress.js
+++ src/pages/courses/application/FormProgress.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Step, Stepper } from 'react-form-stepper';
-import './FormProgress.scss';
-import { useState } from 'react';
+import React from "react";
+import { Step, Stepper } from "react-form-stepper";
+import "./FormProgress.scss";
 
 const status = [
-  'Assessment Passed',
-  'Assessment Failed',
-  'Application In Review',
-  'Application Rejected',
-  'Application Approved',
-  'Payment Failed',
-  'Payment Successfull',
-  'Enrolment Rejected',
-  'Enrolment Approved',
-  'Enrolment Pending',
+  "Assessment Passed",
+  "Assessment Failed",
+  "Application In Review",
+  "Application Rejected",
+  "Application Approved",
+  "Payment Failed",
+  "Payment Successfull",
+  "Enrolment Rejected",
+  "Enrolment Approved",
+  "Enrolment Pending"
 ];
 
-const MultiStepBar = ({ page, onPageNumberClick, testResults, courseTitle }) => {
-  // console.log("courseTitle", courseTitle);
+const MultiStepBar = ({
+  page,
+  onPageNumberClick,
+  testResults,
+  courseTitle
+}) => {
   const hasTakenEntranceTest = status.includes(testResults?.applicationStatus);
+
 
   const onStepperClick = (page) => {
     onPageNumberClick(page);
@@ -28,14 +32,16 @@ const MultiStepBar = ({ page, onPageNumberClick, testResults, courseTitle }) => 
   const fullStackWebDevelopmentCourse = "Full Stack Web Development";
   const industryReadyProgram = "Industry Ready Program";
 
+  
   return (
     <>
-
-
       {courseTitle === fullStackWebDevelopmentCourse && (
-        <Stepper activeStep={page}>
+        <Stepper activeStep={ page}>
           <Step onClick={() => onStepperClick(0)} label="Personal Details" />
-          <Step onClick={() => onStepperClick(1)} label="Education & Work Details" />
+          <Step
+            onClick={() => onStepperClick(1)}
+            label="Education & Work Details"
+          />
           <Step onClick={() => onStepperClick(2)} label="Entrance Test" />
           <Step onClick={() => onStepperClick(3)} label="Test Result" />
           <Step onClick={() => onStepperClick(4)} label="Application Status" />
@@ -43,24 +49,22 @@ const MultiStepBar = ({ page, onPageNumberClick, testResults, courseTitle }) => 
           <Step onClick={() => onStepperClick(5)} label="Payment" />
           <Step onClick={() => onStepperClick(6)} label="KYC & Documents" />
           <Step onClick={() => onStepperClick(7)} label="Enrollment Status" />
-
         </Stepper>
       )}
 
-      {(courseTitle === jobReadyProgramCourse || courseTitle === industryReadyProgram) && (
-
-        <Stepper activeStep={page} >
-
+      {(courseTitle === jobReadyProgramCourse ||
+        courseTitle === industryReadyProgram) && (
+        <Stepper activeStep={page}>
           <Step onClick={() => onStepperClick(0)} label="Personal Details" />
-          <Step onClick={() => onStepperClick(1)} label="Education & Work Details" />
+          <Step
+            onClick={() => onStepperClick(1)}
+            label="Education & Work Details"
+          />
           <Step onClick={() => onStepperClick(2)} label="Payment" />
           <Step onClick={() => onStepperClick(3)} label="KYC & Documents" />
           <Step onClick={() => onStepperClick(4)} label="Enrollment Status" />
         </Stepper>
-
       )}
-
-
     </>
   );
 };
